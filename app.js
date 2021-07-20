ba3fada596775c84988e5fcddb7aaecb9685856f
关淮 <guanhuai@guanhuais-MacBook-Pro.local>
2021-07-20 10:13:31 +0800

    Added files adn changes requied for depolyment to heroku

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var compression = require('compression');
 var helmet = require('helmet');
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Guan:nbhhd321_@cluster0.zcna6.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Guan:nbhhd321_@cluster0.zcna6.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
